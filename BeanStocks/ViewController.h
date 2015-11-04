d6236705c40dc06ba8660da46b5cc5781958d82a
Ehsan Jahromi <ehsan.enj@gmail.com>
2015-11-03 18:30:58 -0800

    Initial Commit

---
+++ BeanStocks/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BeanStocks
+//
+//  Created by Ehsan on 11/3/15.
+//  Copyright © 2015 Ehsan Jahromi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
