a2e4618bfc8ab6b5d9a10e3b35cc565f69300412
Ehsan Jahromi <ehsan.enj@gmail.com>
2015-11-03 19:31:30 -0800

    AddBeanVC added

---
+++ BeanStocks/AddBeanViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddBeanViewController.h
+//  BeanStocks
+//
+//  Created by Ehsan on 11/3/15.
+//  Copyright © 2015 Ehsan Jahromi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddBeanViewController : UIViewController
+
+@end
