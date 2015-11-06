00aa9791a333c5cdaccbd802124129903a34433b
Ehsan Jahromi <ehsan.enj@gmail.com>
2015-11-05 17:58:18 -0800

    signup working

--- BeanStocks/SignUpViewController.m
+++ BeanStocks/SignUpViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "SignUpViewController.h"
+#import <Parse/Parse.h>
 
 @interface SignUpViewController ()
 @property (weak, nonatomic) IBOutlet UITextField *usernameField;
@@ -23,6 +24,29 @@
 }
 
 - (IBAction)onSignUpButtonTapped:(UIButton *)sender {
+    if ([self.usernameField hasText] && [self.passwordField hasText] && [self.emailField hasText]) {
+        PFUser *user = [PFUser user];
+        user.username = self.usernameField.text;
+        user.password = self.passwordField.text;
+        user.email =self.emailField.text;
+        
+        [user signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * _Nullable error) {
+            if (error) {
+                UIAlertController *controller = [UIAlertController alertControllerWithTitle:@"Error" message:@"Sorry there was an error. Please try again later!" preferredStyle:UIAlertControllerStyleAlert];
+                UIAlertAction *action = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
+                [controller addAction:action];
+                [self presentViewController:controller animated:YES completion:nil];
+            }else{
+                [self dismissViewControllerAnimated:YES completion:nil];
+            }
+        }];
+        
+    }else{
+        UIAlertController *controller = [UIAlertController alertControllerWithTitle:@"Error" message:@"Some of the information is missing. Try again!" preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction *action = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
+        [controller addAction:action];
+        [self presentViewController:controller animated:YES completion:nil];
+    }
 }
 
 
